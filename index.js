5a945822f3f07e798629487ce20f154a16a21f88
iamcal77 <+>
2023-06-27 11:14:16 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution in this file!
+// index.js
+
+var customerName = "bob";
+
+
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer(newCustomer) {
+    bestCustomer = newCustomer;
+  }
+ // index.js
+
+const leastFavoriteCustomer = 'initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value'; // This line will throw an error
+}
+ 
